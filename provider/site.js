823cd386f64c79a97dd4359f8355e36e88c539d3
Marco Bettiolo <marco@bettiolo.it>
2015-10-08 10:50:58 +0100

    ES6

--- provider/site.js
+++ provider/site.js
@@ -1,28 +1,20 @@
-/**
- * Module dependencies.
- */
-var passport = require('passport')
-    , login = require('connect-ensure-login')
+import passport from 'passport';
+import login from 'connect-ensure-login';
 
+export default {
+  index: (req, res) => res.send('OAuth 2.0 Server'),
 
-exports.index = function(req, res) {
-    res.send('OAuth 2.0 Server');
-};
+  loginForm: (req, res) => res.render('login'),
 
-exports.loginForm = function(req, res) {
-    res.render('login');
-};
+  login: passport.authenticate('local', { successReturnToOrRedirect: '/', failureRedirect: '/login' }),
 
-exports.login = passport.authenticate('local', { successReturnToOrRedirect: '/', failureRedirect: '/login' });
-
-exports.logout = function(req, res) {
+  logout: (req, res) => {
     req.logout();
     res.redirect('/');
-}
+  },
 
-exports.account = [
+  account: [
     login.ensureLoggedIn(),
-    function(req, res) {
-        res.render('account', { user: req.user });
-    }
-]
+    (req, res) => res.render('account', { user: req.user }),
+  ],
+};
