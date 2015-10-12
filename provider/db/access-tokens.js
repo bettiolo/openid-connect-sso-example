ee45441469392c115840d754d0072cb265037350
Marco Bettiolo <marco@bettiolo.it>
2015-10-12 19:00:41 +0100

    formatting

--- provider/db/access-tokens.js
+++ provider/db/access-tokens.js
@@ -4,7 +4,7 @@ export default {
   find: (key, cb) => cb(null, tokens[key]),
 
   save: (token, userID, clientID, cb) => {
-    tokens[token] = {userID: userID, clientID: clientID};
+    tokens[token] = { userID: userID, clientID: clientID };
     return cb();
   },
 };
