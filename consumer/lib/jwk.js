c6b9ac2dc9e7e2171e4b99e26850debdc8879a25
Marco Bettiolo <marco@bettiolo.it>
2015-10-14 11:11:35 +0100

    Formatting changes

--- consumer/lib/jwk.js
+++ consumer/lib/jwk.js
@@ -2,7 +2,7 @@ import getPem from 'rsa-pem-from-mod-exp';
 
 export default {
   getPem(jwks, kid) {
-    const { n: modulus, e: exponent } = jwks.find(jwk => jwk.kid === kid);
+    const { n: modulus, e: exponent } = jwks.find((jwk) => jwk.kid === kid);
     return getPem(modulus, exponent);
   },
 };
