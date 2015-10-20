d7b8c8b88e2095ab6e92a48c9ef4115ec08983d2
Marco Bettiolo <marco@bettiolo.it>
2015-10-20 12:57:37 +0100

    Simple generation of random tokens

--- provider/endpoints/token.js
+++ provider/endpoints/token.js
@@ -1,26 +1,10 @@
 import crypto from 'crypto';
 const tokenBytes = 256;
 
-export function createTokenSha(cb) {
-  crypto.randomBytes(tokenBytes, (ex, buf) => {
-    if (ex) { return cb(ex); }
-
-    const token = crypto
-      .createHash('sha1')
-      .update(buf)
-      .digest('hex');
-
-    cb(false, token);
-  });
-}
-
-export function createToken(cb) {
-  crypto.randomBytes(tokenBytes, (ex, buf) => {
-    cb(null, buf.toString('hex'));
-  });
-}
-
 export default {
-  createTokenSha,
-  createToken,
+  create(cb) {
+    crypto.randomBytes(tokenBytes, (ex, buf) => {
+      cb(null, buf.toString('hex'));
+    });
+  },
 };
