c83b2377f3340aabf892ac360a059f1da04d8365
Marco Bettiolo <marco@bettiolo.it>
2015-10-20 12:57:49 +0100

    fixed formatting

--- provider/endpoints/userinfo-endpoint.js
+++ provider/endpoints/userinfo-endpoint.js
@@ -3,7 +3,7 @@ import passport from 'passport';
 // Implementation of http://openid.net/specs/openid-connect-core-1_0.html#UserInfo
 
 export default [
-  passport.authenticate('bearer', {session: false}),
+  passport.authenticate('bearer', { session: false }),
   (req, res) => {
     // req.authInfo is set using the `info` argument supplied by
     // `BearerStrategy`. It is typically used to indicate scope of the token,
